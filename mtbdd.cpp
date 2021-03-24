bafdb5664f7f81ab71778539780b3ce5b243fa42
codeboy <michal.sk.com@gmail.com>
2021-03-24 19:13:14 +0100

    Added implementation of my abstract_exists.

--- mtbdd.cpp
+++ mtbdd.cpp
@@ -6,6 +6,7 @@
 #include <sylvan_gmp.h>
 #include <sylvan_mt.h>
 #include <sylvan_mtbdd.h>
+#include <sylvan_int.h>
 #include <sylvan_stats.h>
 
 #include <algorithm>
@@ -67,7 +68,6 @@ static void mk_set_leaf(uint64_t *state_set_ptr) {
 
 static void destroy_set_leaf(uint64_t leaf_value) {
   set<int> *states_set_ptr = (set<int> *)leaf_value;
-  cout << "Delete called on: ";
   print_states_set(states_set_ptr);
   delete states_set_ptr;
 }
@@ -81,7 +81,6 @@ int set_leaf_equals(uint64_t a_ptr, uint64_t b_ptr) {
 
 static uint64_t set_leaf_hash(const uint64_t contents_ptr,
                               const uint64_t seed) {
-  cout << "Hashing!" << endl;
   set<int> *states = (set<int> *)contents_ptr;
   const uint64_t prime = 1099511628211;
   uint64_t hash = seed;
@@ -129,6 +128,131 @@ TASK_IMPL_2(MTBDD, set_union, MTBDD *, pa, MTBDD *, pb) {
   // TODO: Utilize operation cache.
   return mtbdd_invalid;
 }
+
+TASK_DECL_2(MTBDD, my_exists_op, MTBDD *, MTBDD *);
+TASK_IMPL_2(MTBDD, my_exists_op, MTBDD *, pa, MTBDD *, pb) {
+    cout << "Calling my_exists_op" << endl;
+    return CALL(set_union, pa, pb);
+}
+
+/**
+ * The *abstraction* F definition.
+ * Task returns a MTBDD. Task accepts two MTBDDs - left and right child (subtree) of a
+ * node for variable <v> specified in variable set passed to abstract_apply with this operator.
+ * The final int is a number of variables that are missing when reaching a leaf in MTBDD, but there is 
+ * still <k> number of variables in the variable set passed to abstract_apply.
+ */
+TASK_DECL_3(MTBDD, my_abstract_exists_op, MTBDD, MTBDD, int);
+TASK_IMPL_3(MTBDD, my_abstract_exists_op, MTBDD, a, MTBDD, b, int, k) {
+    if (k == 0) {
+        cout << "K is zero, returning my_exists_op" << endl;
+        return CALL(set_union, &a, &b);
+    } else {
+        cout << "K is not zero, returning my_exists_op" << endl;
+        return CALL(set_union, &a, &b);
+    }
+}
+
+TASK_DECL_3(MTBDD, my_abstract_exists, MTBDD, MTBDD, MTBDD);
+TASK_IMPL_3(MTBDD, my_abstract_exists, MTBDD, a, MTBDD, b, MTBDD, v) {
+    cout << "Inside my_abstract_exists" << endl;
+	// Check if there are any more variables in <v>
+    if (v == mtbdd_true) {
+		// We have exhausted the variables in <v>, no abstraction will be applied down the tree.
+		// Just apply the ordinary operator.
+        cout << "The variables <v> are exhausted" << endl;
+		return mtbdd_apply(a, b, TASK(mtbdd_op_times));
+	}
+
+	// Maybe a or b are either terminals (true,false) or leaf nodes. Check
+	// this case by trying to apply the operator, which will return mtbdd_invalid
+	// if it cannot be applied directly to nodes, and therefor the nodes are not
+	// leaves or terminals.
+    cout << "Variables <v> are not empty, trying to apply my_exists_op" << endl;
+    MTBDD result = CALL(my_exists_op, &a, &b);
+    if (result != mtbdd_invalid) {
+        cout << "my_exists_op did not return mtbdd_invalid" << endl;
+        mtbdd_refs_push(result);
+        result = mtbdd_abstract(result, v, TASK(my_abstract_exists_op));
+        mtbdd_refs_pop(1);
+        return result;
+    }
+
+    // At this point we know, that:
+	// 	- v is not a constant (contains some variables)  and
+    // 	- a or b is not a constant (are subtrees)
+
+    int is_a_leaf = mtbdd_isleaf(a);
+    int is_b_leaf = mtbdd_isleaf(b);
+    // If the <a> or <b> are not leaves, then they represent a whole MTBDD, but we want only top node.
+    // Get the top node.
+    mtbddnode_t na = is_a_leaf ? 0 : MTBDD_GETNODE(a);
+    mtbddnode_t nb = is_b_leaf ? 0 : MTBDD_GETNODE(b);
+
+    uint32_t variable_a = is_a_leaf ? 0xffffffff : mtbddnode_getvariable(na);
+    uint32_t variable_b = is_b_leaf ? 0xffffffff : mtbddnode_getvariable(nb);
+
+    // The top nodes of the two subtrees might not be at the same level (might be different vars)
+    // Get the one that is higher in the tree / variable order.
+    uint32_t var = variable_a < variable_b ? variable_a : variable_b;
+
+    // Get the topmost variable from the <v> variable set.
+    mtbddnode_t nv = MTBDD_GETNODE(v);
+    uint32_t variable_v = mtbddnode_getvariable(nv);
+
+    if (variable_v < var) {
+        cout << "The current variable from <v> is sooner than the one inside tree:" << endl;
+        cout << "Variable from <v>:" << variable_v << ", tree var: " << var << endl;
+        // We traversed deeper to the tree that the first variable specified in the variable set <v>.
+        // What to do about it? We continue recursing even deeper, however after leaving the recursion
+        // we must perform the abstraction -- with arguments (result, result) -- both subtrees of the 
+        // imaginary missing variable `variable_v` are the same trees = `result`
+        
+        // Recurse deeper with the next variable from the variable set. 
+        result = CALL(my_abstract_exists, a, b, node_gethigh(v, nv));
+        mtbdd_refs_push(result);
+        result = mtbdd_apply(result, result, TASK(my_exists_op)); // TODO: Change the operand here.
+        mtbdd_refs_pop(1);
+    } else  {
+        MTBDD alow, ahigh, blow, bhigh;
+        // TODO: Rewrite this to normal condition.
+
+        // If <a> is a leaf then it does not have successors - both successors must be <a>
+        alow  = (!is_a_leaf && variable_a == var) ? node_getlow(a, na)  : a;
+        ahigh = (!is_a_leaf && variable_a == var) ? node_gethigh(a, na) : a;
+
+        // If <b> is a leaf then it does not have successors - both successors must be <b>
+        blow  = (!is_b_leaf && variable_b == var) ? node_getlow(b, nb)  : b;
+        bhigh = (!is_b_leaf && variable_b == var) ? node_gethigh(b, nb) : b;
+
+        // Check if we currently processing one of the desired variables.
+        if (variable_v == var) {
+            cout << "Variable from <v> and current tree var are on the same level" << endl;
+            // Step down the recursion and since we are dealing with the desired variable to be
+            // abstracted away we call the operation on it.
+            mtbdd_refs_spawn(SPAWN(my_abstract_exists, ahigh, bhigh, node_gethigh(v, nv)));
+            MTBDD low = mtbdd_refs_push(CALL(my_abstract_exists, alow, blow, node_gethigh(v, nv)));
+            MTBDD high = mtbdd_refs_push(mtbdd_refs_sync(SYNC(my_abstract_exists)));
+            result = CALL(mtbdd_apply, low, high, TASK(mtbdd_op_plus)); // TODO: Change the op here.
+            mtbdd_refs_pop(2);
+        } else  {
+            cout << "Variable from <v> is deeper than the current one:" << endl;
+            cout << "Variable from <v>:" << variable_v << ", tree var: " << var << endl;
+            // variable_v > var (other possibilities were checked)
+            // This means that we are somewhere higher in the tree, and we didn't reach the required variable yet.
+            // The action is to go low via both subtrees and hight via both subtrees and just make 
+            // new node out of resuts.
+            mtbdd_refs_spawn(SPAWN(my_abstract_exists, ahigh, bhigh, v));
+            MTBDD low = mtbdd_refs_push(CALL(my_abstract_exists, alow, blow, v));
+            MTBDD high = mtbdd_refs_sync(SYNC(my_abstract_exists));
+            mtbdd_refs_pop(1);
+            result = mtbdd_makenode(var, low, high);
+        }
+    }
+
+    return result;
+}
+
 
 VOID_TASK_0(simple_fn) {
   // Define custom leaf type
@@ -193,7 +317,6 @@ VOID_TASK_0(simple_fn) {
     MTBDD root_low = mtbdd_getlow(tfn_root);
     assert(mtbdd_isleaf(root_low));
     set<int> *root_low_states = (set<int> *)mtbdd_getvalue(root_low);
-    cout << "Root (0, *): ";
     print_states_set(root_low_states);
   }
 
@@ -205,15 +328,12 @@ VOID_TASK_0(simple_fn) {
       MTBDD root_high_high = mtbdd_gethigh(root_high);
       assert(mtbdd_isleaf(root_high_high));
       set<int> *root_low_states = (set<int> *)mtbdd_getvalue(root_high_high);
-      cout << "Root (1, 1): ";
       print_states_set(root_low_states); // This should be {3, 4}
-      cout << "Refs count: " << sylvan_count_refs() << endl;
     }
     {
       MTBDD root_high_low = mtbdd_getlow(root_high);
       assert(mtbdd_isleaf(root_high_low));
       set<int> *root_low_states = (set<int> *)mtbdd_getvalue(root_high_low);
-      cout << "Root (1, 0): ";
       print_states_set(root_low_states);
     }
   }
